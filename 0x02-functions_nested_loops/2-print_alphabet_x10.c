d5a11770fbf7df02a2809b27c56d7bb2d528f309
Psicolipat <gaxown.sbayka@outlook.com>
2023-03-21 03:01:52 -0700

    alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,8 +3,8 @@
 /**
  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line
- * Return 0
- */
+ * Return 0 
+*/
 void print_alphabet_x10(void)
 {
 	char ch;
@@ -14,7 +14,13 @@ void print_alphabet_x10(void)
 
 	while (i < 10)
 	{
-	        print_alphabet();
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
 		i++;
 	}
 }
